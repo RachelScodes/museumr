d9526f6f40e7b474698497c352a8d4fc1c703ecb
Trevor Dunn <trevor.h.dunn4@gmail.com>
2015-11-19 12:14:24 -0500

    created artist.js model

--- models/artist.js
+++ models/artist.js
@@ -1,3 +1,50 @@
-hi?
+'use strict';
+let mongoose = require('mongoose');
 
-test
+// ~~~~ END SETUP
+
+let artistSchema = new mongoose.Schema({
+	// name
+	name: String,
+	// img_url
+	img_url: String,
+	// nationality
+	nationality: String,
+	// birthYear
+	birthYear: String,
+	// description
+	description: String,
+
+	created_at: Date,
+
+	updated_at: Date
+
+});
+
+artistSchema.pre('save', (next) => {
+	let now = new Data();
+
+	this.updated_at = now;
+	if ( !this.created_at ){
+		this.created_at = now;
+	}
+	next();
+});
+
+let Artist = mongoose.model('Artist', artistSchema);
+
+module.exports = Artist;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
