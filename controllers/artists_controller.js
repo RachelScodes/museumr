d9c6bdd8550385445545603dd4fbddb343b837e2
Anita Growsoon <anitagrowsoon@gmail.com>
2015-11-20 00:25:01 -0500

    more work on starting routes for artists

--- controllers/artists_controller.js
+++ controllers/artists_controller.js
@@ -19,19 +19,39 @@ router.route('/')
         next();
      })
   });
+  .post((req, res, next) => {
+     // add a new artist
+     console.log('create an artist');
+  })
 
-// // add a new artist
-// router.route('/new')
-//   .post((req, res, next) => {
-//     console.log('hit artists/new')
-//     let artist = new Artist({data?})
-//
-//     artist.save((err) => {
-//       (err)? res.send(err) : res.send(artist);
-//       next();
-//      })
-//   })
+// will be hit by clicking div
+router.route('/:id')
+  .get((req, res, next) => {
+    let artistObjectId = req.params.id
+    console.log('show one artist with this id: ' + artistObjectId);
+    Artist.find({_id: artistObjectId }, (err, result) => {
+      // Get paintings by that Artist
+      Painting.find({"_id": {$in: result[0].paintings}}, (error, paintings) => {
+      result.push(paintings)
+      res.send(result);
+      next();
+    })
+  });
 
+// will be hit by typing manually
+// don't forget %20's!
+router.route('/:name')
+  .get((req, res, next) => {
+    let artistName = req.params.name
+    console.log('show one artist with this name: ' + artistName);
+    Artist.find({name: artistName }, (err, result) => {
+      // Get paintings by that Artist
+      Painting.find({"_id": {$in: result[0].paintings}}, (error, paintings) => {
+      result.push(paintings)
+      res.send(result);
+      next();
+    })
+  });
 
 
 // let a sistah know whassup
