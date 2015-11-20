f7c4ac7718669089b30a50db731157f6ae019def
Anita Growsoon <anitagrowsoon@gmail.com>
2015-11-20 00:41:37 -0500

    create routes

--- controllers/artists_controller.js
+++ controllers/artists_controller.js
@@ -13,46 +13,28 @@ let Artist = require('../models/Artist');
 router.route('/')
   .get((req, res, next) => {
      //show all artists
-     console.log('hit /artists');
+     console.log('hit get /artists - show all');
      Artist.find({}, function(err, data) {
         res.send(data);
         next();
      })
   });
   .post((req, res, next) => {
-     // add a new artist
-     console.log('create an artist');
-  })
-
-// will be hit by clicking div
-router.route('/:id')
-  .get((req, res, next) => {
-    let artistObjectId = req.params.id
-    console.log('show one artist with this id: ' + artistObjectId);
-    Artist.find({_id: artistObjectId }, (err, result) => {
-      // Get paintings by that Artist
-      Painting.find({"_id": {$in: result[0].paintings}}, (error, paintings) => {
-      result.push(paintings)
-      res.send(result);
-      next();
-    })
+     //create new artist
+     console.log('hit post /artists - add artist')
+     let newArtist = new Artist({
+       // TODO get this from the html?
+       // name: req.body.name,
+       // img_url: req.body.img_url,
+       // nationality: req.body.nationality,
+       // birthYear: req.body.birthYear,
+       // description: req.body.description
+     });
+     newArtist.save((err) => {
+       (err) ? res.send(err) : res.send(newArtist);
+       next();
+     });
   });
-
-// will be hit by typing manually
-// don't forget %20's!
-router.route('/:name')
-  .get((req, res, next) => {
-    let artistName = req.params.name
-    console.log('show one artist with this name: ' + artistName);
-    Artist.find({name: artistName }, (err, result) => {
-      // Get paintings by that Artist
-      Painting.find({"_id": {$in: result[0].paintings}}, (error, paintings) => {
-      result.push(paintings)
-      res.send(result);
-      next();
-    })
-  });
-
 
 // let a sistah know whassup
 module.exports = router;
