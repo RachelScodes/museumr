b73e57fda1f868903892b68077494cf5b1bbff69
Trevor Dunn <trevor.h.dunn4@gmail.com>
2015-11-19 19:09:38 -0500

    work on index.html and app.js client views

--- public/app.js
+++ public/app.js
@@ -1,7 +1,43 @@
 'use strict';
+
 $(() => {
 
-	let renderTemplate_
+	let renderTemplate_artists = Handlebars.compile($('template#artist-template').html());
+
+
+	let renderTemplate_artists = Handlebars.compile($('template#painting-template').html());
+
+	let renderTemplate_artists = Handlebars.compile($('template#artist-form-template').html());
+
+	let renderTemplate_artists = Handlebars.compile($('template#artist-edit-template').html());
+
+	let renderTemplate_artists = Handlebars.compile($('template#painting-form-template').html());
+
+
+	// Artists --
+	$('.artist-link').click
+
+	// Paintings --
+	$('.paintings-link').
+
+	// Add Artist
+	$('.new-artist')
+
+	// Add Painting
+	$('.new-painting')
+
+
+/*      <li class="artist-link">Artists</li>
+      <li class="paintings-link">Paintings</li>
+      <li class="new-artist">Add Artist</li>
+      <li class="new-painting">Add Painting</li>*/
+
+
+
+
+
+
+
 
 
 
